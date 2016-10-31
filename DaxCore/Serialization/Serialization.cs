ad511e8930b5d3e2455765c4c60086918217742a
Armienn <nigathr@gmail.com>
2016-10-31 12:30:47 +0100

    Implemented SaveNetwork

---
+++ DaxCore/Serialization/Serialization.cs
@@ -0,0 +1,28 @@
+﻿using DaxCore.Networks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaxCore.Serialization
+{
+	class Serialization
+	{
+		public static void SaveNetwork(Network network, string file)
+		{
+			IFormatter formatter = new BinaryFormatter();
+			Stream stream = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None);
+			formatter.Serialize(stream, network);
+			stream.Close();
+		}
+
+		public static Network LoadNetwork(string file)
+		{
+			return null;
+		}
+	}
+}
