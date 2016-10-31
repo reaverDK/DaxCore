ac8673b6241aa5a33cfba7d0c4dea14fd9b3eaa9
reaverDK <reaverdk@live.dk>
2016-10-31 12:00:50 +0100

    initial commit

---
+++ DaxCore/Layers/ActivationLayer.cs
@@ -0,0 +1,52 @@
+﻿using DaxCore.ActivationFunctions;
+using DaxCore.Neurons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaxCore.Layers
+{
+	/// <summary>
+	/// Activation layer
+	/// </summary>
+	/// 
+	/// <remarks>Activation layer is a layer of <see cref="ActivationNeuron">activation neurons</see>.
+	/// The layer is usually used in multi-layer neural networks.</remarks>
+	///
+	public class ActivationLayer : Layer
+	{
+		/// <summary>
+		/// Layer's neurons accessor
+		/// </summary>
+		/// 
+		/// <param name="index">Neuron index</param>
+		/// 
+		/// <remarks>Allows to access layer's neurons.</remarks>
+		/// 
+		public new ActivationNeuron this[int index]
+		{
+			get { return (ActivationNeuron)neurons[index]; }
+		}
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ActivationLayer"/> class
+		/// </summary>
+		/// <param name="neuronsCount">Layer's neurons count</param>
+		/// <param name="inputsCount">Layer's inputs count</param>
+		/// <param name="function">Activation function of neurons of the layer</param>
+		/// 
+		/// <remarks>The new layet will be randomized (see <see cref="ActivationNeuron.Randomize"/>
+		/// method) after it is created.</remarks>
+		/// 
+		public ActivationLayer(int neuronsCount, int inputsCount, IActivationFunction function)
+							: base(neuronsCount, inputsCount)
+		{
+			// create each neuron
+			for (int i = 0; i < neuronsCount; i++)
+				neurons[i] = new ActivationNeuron(inputsCount, function);
+		}
+	}
+}
---
+++ DaxCore/Learning/ISuperVisedLearning.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaxCore.Learning
+{
+	/// <summary>
+	/// Supervised learning interface
+	/// </summary>
+	/// 
+	/// <remarks>The interface describes methods, which should be implemented
+	/// by all supervised learning algorithms. Supervised learning is such a
+	/// type of learning algorithms, where system's desired output is known on
+	/// the learning stage. So, given sample input values and desired outputs,
+	/// system should adopt its internals to produce correct (or closer to correct)
+	/// result after the learning step is complete.</remarks>
+	/// 
+	public interface ISupervisedLearning
+	{
+		/// <summary>
+		/// Runs learning iteration
+		/// </summary>
+		/// 
+		/// <param name="input">input vector</param>
+		/// <param name="output">desired output vector</param>
+		/// 
+		/// <returns>Returns learning error</returns>
+		/// 
+		double Run(double[] input, double[] output);
+
+		/// <summary>
+		/// Runs learning epoch
+		/// </summary>
+		/// 
+		/// <param name="input">array of input vectors</param>
+		/// <param name="output">array of output vectors</param>
+		/// 
+		/// <returns>Returns sum of learning errors</returns>
+		/// 
+		double RunEpoch(double[][] input, double[][] output);
+	}
+}
