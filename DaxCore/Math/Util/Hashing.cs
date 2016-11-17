2685a2d0b121b128b079fa5af7d7a600469871c0
Thomas Busborg Nørgaard <thom383x@edu.eal.dk>
2016-11-17 12:30:38 +0100

    Added hashing

---
+++ DaxCore/Math/Util/Hashing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaxCore.Math.Util
+{
+    class Hashing
+    {
+        public double HashAlgorithm(string input)
+        {
+            if (input != "")
+            {
+                string inputLower = input.ToLower();
+                double hash = 0;
+                char[] c = inputLower.ToCharArray();
+                byte[] unicodebytes = Encoding.Unicode.GetBytes(c);
+                List<int> num = new List<int>();
+                for (int j = 0; j < unicodebytes.Length; j++)
+                {
+                    num.Add(unicodebytes[j] + (256 * unicodebytes[j + 1]));
+                    j++;
+                }
+                double x = num.OrderBy(nums => nums).Reverse().ToList()[0];
+                for (int i = 0; i < num.Count; i++)
+                {
+                    double l = (double)x / (double)unicodebytes.Length;
+                    hash += (((double)num[i] * (1.0 / (i + 1))) + ((double)num[num.Count - (i + 1)] * (1.0 / (i + 1))) * (l * 0.0001));
+                    x = (double)hash;
+                }
+                return hash;
+            }
+            return 0;
+        }
+    }
+}
